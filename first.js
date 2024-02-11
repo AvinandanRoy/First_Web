cf86a2c218daea1be00c5db386ee0c1f8852216d
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-11 23:34:54 +0600

    Last update

--- first.js
+++ first.js
@@ -1,1 +1,6 @@
-console.log("My name is Avinandan Roy");
+const person ={
+    name : "Avinandan Roy",
+    age : 22 
+}
+
+console.log(person.name)
---
+++ first.py
@@ -0,0 +1,8 @@
+
+a = int(input())
+a = int(input())
+
+def mySummation(a , b ) :
+    return a+b 
+
+print(mySummation1)
